ed7f076276c7c943d66a9fba98e8afd68201da69
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-07-15 15:36:29 +0530

    Created a dummy task and passed it as prop to Main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 import "../src/styles/App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { title: "Task 2", description: "Description 2", id: "1" },
+    { title: "Task 1", description: "Description 1", id: "2" },
+    { title: "Task 3", description: "Description 3", id: "3" },
+    { title: "Task 4", description: "Description 4", id: "4" },
+    { title: "Task 5", description: "Description 5", id: "5" },
+  ]);
+
   return (
-    <div>
+    <div className="app">
       <Header />
+      <div className="content">
+        <SideBar />
+        <Main tasks={tasks} />
+      </div>
+      <Footer />
     </div>
   );
 }

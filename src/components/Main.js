4ee607cfbcc68321bdaca727690b30ccb9d9bb3c
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-07-15 17:31:28 +0530

    Removed div surrounding the tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,9 @@ import "../styles/Main.css";
 const Main = ({ tasks }) => {
   return (
     <main>
-      <div>
-        {tasks.map((task) => {
-          return <Task task={task} />;
-        })}
-      </div>
+      {tasks.map((task) => {
+        return <Task task={task} />;
+      })}
     </main>
   );
 };

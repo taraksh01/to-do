a3a1feb47bd4da911e67374c20882946e633f471
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-07-15 19:10:58 +0530

    Gave className of action to div containg icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,18 @@
-import "../styles/Task.css"
+import "../styles/Task.css";
+import trash from "../images/trash.svg";
+import edit from "../images/edit.svg";
+// import complete from "../images/check-circle.svg";
 
 const Task = ({ task }) => {
   return (
     <div className="task">
       <p className="title">{task.title}</p>
       <p className="description">{task.description}</p>
+      <div className="action">
+        <img src={trash} alt="delete" />
+        <img src={edit} alt="edit" />
+        {/* <img src={complete} alt="complete" /> */}
+      </div>
     </div>
   );
 };
